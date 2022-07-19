cf6520d55f65df3c016c027dbd6a8b6cdec8bd13
Micoder-dev <micoder.com@gmail.com>
2022-07-19 08:42:56 +0530

    create retrofit service generator

---
+++ app/src/main/java/com/example/programmingquotes/RetrofitServiceGenerator.java
@@ -0,0 +1,36 @@
+package com.example.programmingquotes;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitServiceGenerator {
+
+    private final static String BASE_API_URL = "https://programming-quotes-api.herokuapp.com/";
+    private static Retrofit mRetrofit = null;
+    private static Gson gson = new GsonBuilder().create();
+
+    //monitor & manage your API calls by logging them
+    private static HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+    private static OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor);
+
+    private static OkHttpClient okHttpClient = okHttpClientBuilder.build();
+
+    public static <T> T createRetrifitService(Class<T> serviceClass) {
+
+        if (mRetrofit == null) {
+            mRetrofit = new Retrofit.Builder()
+                    .client(okHttpClient)
+                    .baseUrl(BASE_API_URL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+        return mRetrofit.create(serviceClass);
+    }
+
+}
